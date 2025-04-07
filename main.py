646d038ad0bd92383fbdfa6b48a5285b038cde52
YingXu-Koi <yingxu2030@gmail.com>
2025-04-07 12:58:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import subprocess
 import speech_recognition as sr
 import streamlit as st
 import uuid
+import time
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import OpenAI
