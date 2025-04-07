b9924ed0a49bf345ff3b5d4cabbf90517d26ae77
YingXu-Koi <yingxu2030@gmail.com>
2025-04-07 14:46:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -291,9 +291,7 @@ def main():
     )
 
     if st.button('Start Voice Input'):
-        user_input = recognize_speech()
-        if user_input:
-            st.text_input('Start the conversation!', value=user_input)
+            st.text_input('Apologies, this feature is currently under maintenance!')
     else:
         user_input = st.text_input('Start the conversation. What would you like to ask?')
     
