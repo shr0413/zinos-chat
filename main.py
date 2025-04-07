518beb38c102d682682d20c6e300c4df791d6457
YingXu-Koi <yingxu2030@gmail.com>
2025-04-07 14:35:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -292,9 +292,10 @@ def main():
 
         
     if st.button('Start Voice Input'):
-         st.text_input = 'Apologies, this feature is currently under maintenance!'
-    else:
-        st.text_input ='Start the conversation. What would you like to ask?'
+        if user_input:
+                st.text_input('Apologies, this feature is currently under maintenance!', value=user_input)
+        else:
+            user_input = st.text_input('Start the conversation. What would you like to ask?')
     
     if user_input:
         vectordb = Chroma(
